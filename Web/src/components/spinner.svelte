93cf3c0b5385f0569e287fc74beef3ec74ee9e81
v79 <liamjdavison@gmail.com>
2023-02-11 12:26:13 +0000

    started a spinner component for loading

---
+++ Web/src/components/spinner.svelte
@@ -0,0 +1,5 @@
+<div class="flex items-center justify-center">
+	<div class="spinner-border inline-block h-8 w-8 animate-spin rounded-full border-4" role="status">
+		<span class="visually-hidden">Loading...</span>
+	</div>
+</div>
