9251e7ca58f8a7a9772e1b1a9d3c8708a251d407
v79 <liamjdavison@gmail.com>
2023-02-25 11:05:02 +0000

    Force the footer to the bottom of the page.

--- Web/src/routes/+page.svelte
+++ Web/src/routes/+page.svelte
@@ -6,32 +6,34 @@
     import SpinnerWrapper from '../components/utilities/spinnerWrapper.svelte';
 </script>
 
-<Navigation />
+<div class="flex h-screen flex-col">
+	<Navigation />
 
-<div class="flex flex-row">
-	<div class="basis-1/4 bg-slate-400">
-		<PostList />
-	</div>
-	<div class="basis-1/2 bg-slate-600">
-		<MarkdownEditor />
-	</div>
-	<div class="invisible basis-1/4 bg-slate-800 lg:visible">
-		<h3 class="px-4 py-4 text-center text-2xl font-bold text-slate-200">Messages</h3>
-		<SpinnerWrapper spinnerID="globalSpinner" />
-	</div>
-</div>
-
-<Footer>
-	<div class="grid md:grid-cols-2 lg:grid-cols-4">
-		<div class="mb-6">
-			<h5 class="mb-2.5 font-bold uppercase text-gray-800">About Cantilever</h5>
-			<FooterLinkGroup>
-				<FooterLink href="https://www.cantilevers.org/">Project History</FooterLink>
-			</FooterLinkGroup>
+	<div class="flex grow flex-row">
+		<div class="basis-1/4 bg-slate-400">
+			<PostList />
+		</div>
+		<div class="basis-1/2 bg-slate-600">
+			<MarkdownEditor />
+		</div>
+		<div class="invisible basis-1/4 bg-slate-800 lg:visible">
+			<h3 class="px-4 py-4 text-center text-2xl font-bold text-slate-200">Messages</h3>
+			<SpinnerWrapper spinnerID="globalSpinner" />
 		</div>
 	</div>
-	<FooterCopyright href="https://www.cantilevers.org/app/" by="Liam Davison" year={2023} />
-</Footer>
+
+	<Footer>
+		<div class="grid md:grid-cols-2 lg:grid-cols-4">
+			<div class="mb-6">
+				<h5 class="mb-2.5 font-bold uppercase text-gray-800">About Cantilever</h5>
+				<FooterLinkGroup>
+					<FooterLink href="https://www.cantilevers.org/">Project History</FooterLink>
+				</FooterLinkGroup>
+			</div>
+		</div>
+		<FooterCopyright href="https://www.cantilevers.org/app/" by="Liam Davison" year={2023} />
+	</Footer>
+</div>
 
 <style lang="postcss">
 	:global(html) {
