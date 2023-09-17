4b3899166e8964336b0a719ea9c6b15ebe567c87
Liam <liamjdavison@gmail.com>
2023-09-17 09:32:01 +0100

    Cleaned up imports

--- SharedModels/src/main/kotlin/org/liamjd/cantilever/models/CantileverProject.kt
+++ SharedModels/src/main/kotlin/org/liamjd/cantilever/models/CantileverProject.kt
@@ -6,7 +6,6 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
