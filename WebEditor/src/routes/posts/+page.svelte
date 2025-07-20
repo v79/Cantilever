49ad1224d189a829acf54bf995e7f267d1dfdada
Liam <liamjdavison@gmail.com>
2025-07-20 07:34:09 +0100

    Fixed corrupted post page

--- WebEditor/src/routes/posts/+page.svelte
+++ WebEditor/src/routes/posts/+page.svelte
@@ -1,4 +1,240 @@
-<script lang="ts" ✂prettier:content✂="CglpbXBvcnQgQmFzaWNGaWxlTGlzdCBmcm9tICckbGliL2NvbXBvbmVudHMvQmFzaWNGaWxlTGlzdC5zdmVsdGUnOwoJaW1wb3J0IFBvc3RMaXN0SXRlbSBmcm9tICckbGliL2NvbXBvbmVudHMvRmlsZUxpc3RJdGVtLnN2ZWx0ZSc7CglpbXBvcnQgTGlzdFBsYWNlaG9sZGVyIGZyb20gJyRsaWIvY29tcG9uZW50cy9MaXN0UGxhY2Vob2xkZXIuc3ZlbHRlJzsKCWltcG9ydCBEYXRlUGlja2VyIGZyb20gJyRsaWIvZm9ybXMvZGF0ZVBpY2tlci5zdmVsdGUnOwoJaW1wb3J0IE1hcmtkb3duRWRpdG9yIGZyb20gJyRsaWIvZm9ybXMvbWFya2Rvd25FZGl0b3Iuc3ZlbHRlJzsKCWltcG9ydCBUZXh0SW5wdXQgZnJvbSAnJGxpYi9mb3Jtcy90ZXh0SW5wdXQuc3ZlbHRlJzsKCWltcG9ydCB7IE1hcmtkb3duQ29udGVudCwgUG9zdEl0ZW0gfSBmcm9tICckbGliL21vZGVscy9tYXJrZG93bic7CglpbXBvcnQgeyBDTEVBUl9NQVJLRE9XTiwgbWFya2Rvd25TdG9yZSB9IGZyb20gJyRsaWIvc3RvcmVzL2NvbnRlbnRTdG9yZS5zdmVsdGUnOwoJaW1wb3J0IHsgcHJvamVjdCB9IGZyb20gJyRsaWIvc3RvcmVzL3Byb2plY3RTdG9yZS5zdmVsdGUnOwoJaW1wb3J0IHsgdXNlclN0b3JlIH0gZnJvbSAnJGxpYi9zdG9yZXMvdXNlclN0b3JlLnN2ZWx0ZSc7CglpbXBvcnQgeyBnZXRNb2RhbFN0b3JlLCBnZXRUb2FzdFN0b3JlLCB0eXBlIFRvYXN0U2V0dGluZ3MsIHR5cGUgVHJlZVZpZXdOb2RlIH0gZnJvbSAnQHNrZWxldG9ubGFicy9za2VsZXRvbic7CglpbXBvcnQgeyBvbk1vdW50LCB0aWNrIH0gZnJvbSAnc3ZlbHRlJzsKCWltcG9ydCB7IEFkZCwgRGVsZXRlLCBJY29uLCBSZWZyZXNoLCBTYXZlIH0gZnJvbSAnc3ZlbHRlLWdvb2dsZS1tYXRlcmlhbGRlc2lnbi1pY29ucyc7CglpbXBvcnQgeyBkZWxldGVQb3N0LCBmZXRjaFBvc3QsIGZldGNoUG9zdHMsIHBvc3RzLCBzYXZlUG9zdCB9IGZyb20gJy4uLy4uL2xpYi9zdG9yZXMvcG9zdFN0b3JlLnN2ZWx0ZSc7CgoJY29uc3QgbW9kYWxTdG9yZSA9IGdldE1vZGFsU3RvcmUoKTsKCWNvbnN0IHRvYXN0U3RvcmUgPSBnZXRUb2FzdFN0b3JlKCk7CgoJJDogd2ViUGFnZVRpdGxlID0gJG1hcmtkb3duU3RvcmUubWV0YWRhdGE/LnRpdGxlID8gJyAtICcgKyAkbWFya2Rvd25TdG9yZS5tZXRhZGF0YT8udGl0bGUgOiAnJzsKCglsZXQgcG9zdExpc3ROb2RlcyA9IFtdIGFzIFRyZWVWaWV3Tm9kZVtdOyAvLyBmb3IgdGhlIHRyZWV2aWV3IGNvbXBvbmVudAoJbGV0IHBnVGl0bGU6IHN0cmluZzsKCSQ6IG1hcmtkb3duVGl0bGUgPSAkbWFya2Rvd25TdG9yZS5tZXRhZGF0YT8udGl0bGUgPz8gJ1VudGl0bGVkJzsKCSQ6IHBvc3RJc1ZhbGlkID0gJG1hcmtkb3duU3RvcmUubWV0YWRhdGE/LnRpdGxlICE9IG51bGwgJiYgJG1hcmtkb3duU3RvcmUubWV0YWRhdGE/LnRpdGxlICE9ICcnOwoJbGV0IGlzTmV3UG9zdCA9IGZhbHNlOwoKCS8vIGRlZmluZSBtb2RhbHMKCS8qKgoJICogQHR5cGU6IHtNb2RhbFNldHRpbmdzfQoJICovCgkkOiBzYXZlUG9zdE1vZGFsID0gewoJCXR5cGU6ICdjb25maXJtJywKCQl0aXRsZTogJ0NvbmZpcm0gc2F2ZScsCgkJYm9keTogIlNhdmUgY2hhbmdlcyB0byBwb3N0ICc8c3Ryb25nPiIgKyBtYXJrZG93blRpdGxlICsgIjwvc3Ryb25nPic/IiwKCQlidXR0b25UZXh0Q29uZmlybTogJ1NhdmUnLAoJCWJ1dHRvblRleHRDYW5jZWw6ICdDYW5jZWwnLAoJCS8vIFRSVUUgaWYgY29uZmlybSBwcmVzc2VkLCBGQUxTRSBpZiBjYW5jZWwgcHJlc3NlZAoJCXJlc3BvbnNlOiAocjogYm9vbGVhbikgPT4gewoJCQlpZiAocikgewoJCQkJaW5pdGlhdGVTYXZlUG9zdCgpOwoJCQl9CgkJCW1vZGFsU3RvcmUuY2xvc2UoKTsKCQl9Cgl9OwoKCS8qKgoJICogQHR5cGU6IHtNb2RhbFNldHRpbmdzfQoJICovCgkkOiBkZWxldGVQb3N0TW9kYWwgPSB7CgkJdHlwZTogJ2NvbXBvbmVudCcsCgkJY29tcG9uZW50OiAnY29uZmlybURlbGV0ZU1vZGFsJywKCQltZXRhOiB7CgkJCW1vZGFsVGl0bGU6ICdDb25maXJtIHBvc3QgZGVsZXRpb24nLAoJCQlpdGVtS2V5OiAkbWFya2Rvd25TdG9yZS5tZXRhZGF0YT8uc3JjS2V5ID8/ICd1bmtub3duJywKCQkJb25Gb3JtU3VibWl0OiAoKSA9PiB7CgkJCQlpbml0aWF0ZURlbGV0ZVBvc3QoKTsKCQkJfQoJCX0KCX07CgoJLyoqCgkgKiBAdHlwZToge01vZGFsU2V0dGluZ3N9CgkgKi8KCSQ6IHNhdmVOZXdQb3N0TW9kYWwgPSB7CgkJdHlwZTogJ2NvbXBvbmVudCcsCgkJY29tcG9uZW50OiAnc2F2ZU5ld1Bvc3RNb2RhbCcsCgkJbWV0YTogewoJCQltb2RhbFRpdGxlOiAnU2F2ZSBuZXcgcG9zdCcsCgkJCXBvc3RUaXRsZTogbWFya2Rvd25UaXRsZSwKCQkJdGVtcGxhdGVLZXk6ICRtYXJrZG93blN0b3JlLm1ldGFkYXRhPy50ZW1wbGF0ZUtleSA/PyAndW5rbm93bicsCgkJCW9uRm9ybVN1Ym1pdDogKCkgPT4gewoJCQkJaW5pdGlhdGVTYXZlUG9zdCgpOwoJCQl9CgkJfQoJfTsKCgkvKioKCSAqIEB0eXBlOiB7TW9kYWxTZXR0aW5nc30KCSAqLwoJJDogY3JlYXRlTmV3UG9zdE1vZGFsID0gewoJCXR5cGU6ICdjb21wb25lbnQnLAoJCWNvbXBvbmVudDogJ2NyZWF0ZU5ld1Bvc3RNb2RhbCcsCgkJbWV0YTogewoJCQltb2RhbFRpdGxlOiAnQ3JlYXRlIG5ldyBwb3N0JywKCQkJb25Gb3JtU3VibWl0OiAoKSA9PiB7CgkJCQlpbml0aWF0ZU5ld1Bvc3QoKTsKCQkJfQoJCX0KCX07CgoJY29uc3QgdG9hc3Q6IFRvYXN0U2V0dGluZ3MgPSB7CgkJbWVzc2FnZTogJ0xvYWRlZCBwb3N0cycsCgkJYmFja2dyb3VuZDogJ3ZhcmlhbnQtZmlsbGVkLXN1Y2Nlc3MnLAoJCWhpZGVEaXNtaXNzOiB0cnVlCgl9OwoJY29uc3QgZXJyb3JUb2FzdDogVG9hc3RTZXR0aW5ncyA9IHsKCQltZXNzYWdlOiAnRmFpbGVkIHRvIGxvYWQgcG9zdHMnLAoJCWJhY2tncm91bmQ6ICd2YXJpYW50LWZpbGxlZC1lcnJvcicKCX07CgoJb25Nb3VudChhc3luYyAoKSA9PiB7CgkJaWYgKHBvc3RzLmlzRW1wdHkoKSkgewoJCQlhd2FpdCBsb2FkUG9zdExpc3QoKTsKCQl9Cgl9KTsKCglhc3luYyBmdW5jdGlvbiBsb2FkUG9zdExpc3QoKSB7CgkJaWYgKCEkdXNlclN0b3JlLnRva2VuIHx8ICRwcm9qZWN0LmRvbWFpbiA9PT0gJycpIHsKCQkJY29uc29sZS5sb2coJ25vIHRva2VuIG9yIGRvbWFpbicpOwoJCQlyZXR1cm47CgkJfSBlbHNlIHsKCQkJbGV0IGZldGNoUmVzdWx0ID0gYXdhaXQgZmV0Y2hQb3N0cygkdXNlclN0b3JlLnRva2VuLCAkcHJvamVjdC5kb21haW4pOwoJCQlpZiAoZmV0Y2hSZXN1bHQgaW5zdGFuY2VvZiBFcnJvcikgewoJCQkJZXJyb3JUb2FzdC5tZXNzYWdlID0gJ0ZhaWxlZCB0byBsb2FkIHBvc3RzLiBNZXNzYWdlIHdhczogJyArIGZldGNoUmVzdWx0Lm1lc3NhZ2U7CgkJCQl0b2FzdFN0b3JlLnRyaWdnZXIoZXJyb3JUb2FzdCk7CgkJCQljb25zb2xlLmVycm9yKGZldGNoUmVzdWx0KTsKCQkJfSBlbHNlIHsKCQkJCXRvYXN0Lm1lc3NhZ2UgPSAnTG9hZGVkICcgKyBmZXRjaFJlc3VsdCArICcgcG9zdHMnOwoJCQkJdG9hc3RTdG9yZS50cmlnZ2VyKHRvYXN0KTsKCQkJfQoJCX0KCX0KCglhc3luYyBmdW5jdGlvbiBpbml0aWF0ZUxvYWRQb3N0KHNyY0tleTogc3RyaW5nKSB7CgkJbGV0IGxvYWRSZXNwb25zZSA9IGZldGNoUG9zdChzcmNLZXksICR1c2VyU3RvcmUudG9rZW4hISwgJHByb2plY3QuZG9tYWluKTsKCQlsb2FkUmVzcG9uc2UudGhlbigocikgPT4gewoJCQlpZiAociBpbnN0YW5jZW9mIEVycm9yKSB7CgkJCQllcnJvclRvYXN0Lm1lc3NhZ2UgPSAnRmFpbGVkIHRvIGxvYWQgcG9zdCc7CgkJCQl0b2FzdFN0b3JlLnRyaWdnZXIoZXJyb3JUb2FzdCk7CgkJCX0gZWxzZSB7CgkJCQl0b2FzdC5tZXNzYWdlID0gcjsKCQkJCXRvYXN0U3RvcmUudHJpZ2dlcih0b2FzdCk7CgkJCQlpc05ld1Bvc3QgPSBmYWxzZTsKCQkJfQoJCX0pOwoJfQoKCWZ1bmN0aW9uIGluaXRpYXRlTmV3UG9zdCgpIHsKCQljb25zb2xlLmxvZygnQ3JlYXRpbmcgbmV3IHBvc3Qgd2l0aCBlbXB0eSBNYXJrZG93bkNvbnRlbnQnKTsKCQlsZXQgbmV3UG9zdCA9IG5ldyBNYXJrZG93bkNvbnRlbnQoCgkJCW5ldyBQb3N0SXRlbSgnJywgJycsICdzb3VyY2VzL3RlbXBsYXRlcy9wb3N0Lmh0bWwuaGJzJywgJycsIG5ldyBEYXRlKCksIG5ldyBEYXRlKCksIHRydWUpLAoJCQknJwoJCSk7CgkJbWFya2Rvd25TdG9yZS5zZXQobmV3UG9zdCk7CgkJaXNOZXdQb3N0ID0gdHJ1ZTsKCX0KCglhc3luYyBmdW5jdGlvbiBpbml0aWF0ZVNhdmVQb3N0KCkgewoJCWlmICgkbWFya2Rvd25TdG9yZS5tZXRhZGF0YSkgewoJCQlsZXQgbWV0YWRhdGEgPSAkbWFya2Rvd25TdG9yZS5tZXRhZGF0YTsKCQkJaWYgKGlzTmV3UG9zdCB8fCBtZXRhZGF0YS5zcmNLZXkgPT09ICcnKSB7CgkJCQljb25zb2xlLmxvZygnSXMgYSBuZXcgcG9zdCAvIHNyY0tleSBpcyBibGFuaywgc28gc2V0dGluZyBpdCB0byB0aGUgc2x1ZycsIG1ldGFkYXRhLnNsdWcpOwoJCQkJbWV0YWRhdGEuc3JjS2V5ID0gJHByb2plY3QuZG9tYWluICsgJy9zb3VyY2VzL3Bvc3RzLycgKyBtZXRhZGF0YS5zbHVnICsgJy5tZCc7CgkJCQkkbWFya2Rvd25TdG9yZS5tZXRhZGF0YSA9IG1ldGFkYXRhOwoJCQl9CgkJCWxldCBzYXZlUmVzdWx0ID0gYXdhaXQgc2F2ZVBvc3QobWV0YWRhdGE/LnNyY0tleSwgJHVzZXJTdG9yZS50b2tlbiEhLCAkcHJvamVjdC5kb21haW4pOwoJCQlpZiAoc2F2ZVJlc3VsdCBpbnN0YW5jZW9mIEVycm9yKSB7CgkJCQllcnJvclRvYXN0Lm1lc3NhZ2UgPSAnRmFpbGVkIHRvIHNhdmUgcG9zdCc7CgkJCQl0b2FzdFN0b3JlLnRyaWdnZXIoZXJyb3JUb2FzdCk7CgkJCX0gZWxzZSB7CgkJCQljb25zb2xlLmxvZygncG9zdFN0b3JlOiBTYXZlZCBwb3N0JywgbWV0YWRhdGEuc3JjS2V5KTsKCQkJCWlzTmV3UG9zdCA9IGZhbHNlOwoJCQkJdG9hc3QubWVzc2FnZSA9IHNhdmVSZXN1bHQ7CgkJCQl0b2FzdFN0b3JlLnRyaWdnZXIodG9hc3QpOwoJCQkJcmVsb2FkUG9zdExpc3QoKTsKCQkJfQoJCX0KCX0KCglhc3luYyBmdW5jdGlvbiBpbml0aWF0ZURlbGV0ZVBvc3QoKSB7CgkJY29uc29sZS5sb2coJ0RlbGV0aW5nIHBvc3QnKTsKCQlpZiAoJG1hcmtkb3duU3RvcmUubWV0YWRhdGEpIHsKCQkJbGV0IGRlbGV0ZVJlc3VsdCA9IGRlbGV0ZVBvc3QoCgkJCQkkbWFya2Rvd25TdG9yZS5tZXRhZGF0YS5zcmNLZXksCgkJCQkkdXNlclN0b3JlLnRva2VuISEsCgkJCQkkcHJvamVjdC5kb21haW4KCQkJKTsKCQkJZGVsZXRlUmVzdWx0LnRoZW4oKHIpID0+IHsKCQkJCWlmIChyIGluc3RhbmNlb2YgRXJyb3IpIHsKCQkJCQllcnJvclRvYXN0Lm1lc3NhZ2UgPSAnRmFpbGVkIHRvIGRlbGV0ZSBwb3N0JzsKCQkJCQl0b2FzdFN0b3JlLnRyaWdnZXIoZXJyb3JUb2FzdCk7CgkJCQl9IGVsc2UgewoJCQkJCXRvYXN0Lm1lc3NhZ2UgPSAnRGVsZXRlZCBwb3N0ICcgKyByOwoJCQkJCW1hcmtkb3duU3RvcmUuc2V0KENMRUFSX01BUktET1dOKTsKCQkJCQl0b2FzdFN0b3JlLnRyaWdnZXIodG9hc3QpOwoJCQkJCXJlbG9hZFBvc3RMaXN0KCk7CgkJCQl9CgkJCX0pOwoJCX0KCX0KCglhc3luYyBmdW5jdGlvbiByZWxvYWRQb3N0TGlzdCgpIHsKCQlwb3N0TGlzdE5vZGVzID0gW107CgkJcG9zdHMuc2V0KHsgY291bnQ6IC0xLCBwb3N0czogW10gfSk7CgkJdGljaygpOwoJCWF3YWl0IGxvYWRQb3N0TGlzdCgpOwoJfQoKCWNvbnN0IHVzZXJTdG9yZVVuc3Vic2NyaWJlID0gdXNlclN0b3JlLnN1YnNjcmliZSgodmFsdWUpID0+IHsKCQlpZiAodmFsdWUpIHsKCQkJLy8gZG8gbm90aGluZwoJCX0KCX0pOwoKCWNvbnN0IHBvc3RzVW5zdWJzY3JpYmUgPSBwb3N0cy5zdWJzY3JpYmUoKHZhbHVlKSA9PiB7CgkJaWYgKHZhbHVlICYmIHZhbHVlLmNvdW50ICE9IC0xKSB7CgkJCS8vIGJ1aWxkIFRyZWVWaWV3Tm9kZXMgZnJvbSBQb3N0Tm9kZXMKCQkJZm9yIChjb25zdCBwb3N0IG9mIHZhbHVlLnBvc3RzKSB7CgkJCQlwb3N0TGlzdE5vZGVzLnB1c2goewoJCQkJCWlkOiBwb3N0LnNyY0tleSwKCQkJCQljb250ZW50OiBQb3N0TGlzdEl0ZW0sCgkJCQkJY29udGVudFByb3BzOiB7IHRpdGxlOiBwb3N0LnRpdGxlLCBkYXRlOiBwb3N0LmRhdGUsIHNyY0tleTogcG9zdC5zbHVnIH0KCQkJCX0pOwoJCQl9CgkJCXBvc3RMaXN0Tm9kZXMgPSBbLi4ucG9zdExpc3ROb2Rlc107CgkJfQoJfSk7CgoJY29uc3QgY29udGVudFN0b3JlVW5zdWJzY3JpYmUgPSBtYXJrZG93blN0b3JlLnN1YnNjcmliZSgodmFsdWUpID0+IHsKCQlpZiAodmFsdWUpIHsKCQkJaWYgKHZhbHVlLm1ldGFkYXRhICE9IG51bGwpIHsKCQkJCXBnVGl0bGUgPSB2YWx1ZS5tZXRhZGF0YS50aXRsZTsKCQkJfQoJCX0KCX0pOwo=">{}</script>
+<script lang="ts">
+	import BasicFileList from '$lib/components/BasicFileList.svelte';
+	import PostListItem from '$lib/components/FileListItem.svelte';
+	import ListPlaceholder from '$lib/components/ListPlaceholder.svelte';
+	import DatePicker from '$lib/forms/datePicker.svelte';
+	import MarkdownEditor from '$lib/forms/markdownEditor.svelte';
+	import TextInput from '$lib/forms/textInput.svelte';
+	import { MarkdownContent, PostItem } from '$lib/models/markdown';
+	import { CLEAR_MARKDOWN, markdownStore } from '$lib/stores/contentStore.svelte';
+	import { project } from '$lib/stores/projectStore.svelte';
+	import { userStore } from '$lib/stores/userStore.svelte';
+	import {
+		getModalStore,
+		getToastStore,
+		type ToastSettings,
+		type TreeViewNode
+	} from '@skeletonlabs/skeleton';
+	import { onMount, tick } from 'svelte';
+	import { Add, Delete, Icon, Refresh, Save } from 'svelte-google-materialdesign-icons';
+	import {
+		deletePost,
+		fetchPost,
+		fetchPosts,
+		posts,
+		savePost
+	} from '../../lib/stores/postStore.svelte';
+
+	const modalStore = getModalStore();
+	const toastStore = getToastStore();
+
+	$: webPageTitle = $markdownStore.metadata?.title ? ' - ' + $markdownStore.metadata?.title : '';
+
+	let postListNodes = [] as TreeViewNode[]; // for the treeview component
+	let pgTitle: string;
+	$: markdownTitle = $markdownStore.metadata?.title ?? 'Untitled';
+	$: postIsValid = $markdownStore.metadata?.title != null && $markdownStore.metadata?.title != '';
+	let isNewPost = false;
+
+	// define modals
+	/**
+	 * @type: {ModalSettings}
+	 */
+	$: savePostModal = {
+		type: 'confirm',
+		title: 'Confirm save',
+		body: "Save changes to post '<strong>" + markdownTitle + "</strong>'?",
+		buttonTextConfirm: 'Save',
+		buttonTextCancel: 'Cancel',
+		// TRUE if confirm pressed, FALSE if cancel pressed
+		response: (r: boolean) => {
+			if (r) {
+				initiateSavePost();
+			}
+			modalStore.close();
+		}
+	};
+
+	/**
+	 * @type: {ModalSettings}
+	 */
+	$: deletePostModal = {
+		type: 'component',
+		component: 'confirmDeleteModal',
+		meta: {
+			modalTitle: 'Confirm post deletion',
+			itemKey: $markdownStore.metadata?.srcKey ?? 'unknown',
+			onFormSubmit: () => {
+				initiateDeletePost();
+			}
+		}
+	};
+
+	/**
+	 * @type: {ModalSettings}
+	 */
+	$: saveNewPostModal = {
+		type: 'component',
+		component: 'saveNewPostModal',
+		meta: {
+			modalTitle: 'Save new post',
+			postTitle: markdownTitle,
+			templateKey: $markdownStore.metadata?.templateKey ?? 'unknown',
+			onFormSubmit: () => {
+				initiateSavePost();
+			}
+		}
+	};
+
+	/**
+	 * @type: {ModalSettings}
+	 */
+	$: createNewPostModal = {
+		type: 'component',
+		component: 'createNewPostModal',
+		meta: {
+			modalTitle: 'Create new post',
+			onFormSubmit: () => {
+				initiateNewPost();
+			}
+		}
+	};
+
+	const toast: ToastSettings = {
+		message: 'Loaded posts',
+		background: 'variant-filled-success',
+		hideDismiss: true
+	};
+	const errorToast: ToastSettings = {
+		message: 'Failed to load posts',
+		background: 'variant-filled-error'
+	};
+
+	onMount(async () => {
+		if (posts.isEmpty()) {
+			await loadPostList();
+		}
+	});
+
+	async function loadPostList() {
+		if (!$userStore.token || $project.domain === '') {
+			console.log('no token or domain');
+			return;
+		} else {
+			let fetchResult = await fetchPosts($userStore.token, $project.domain);
+			if (fetchResult instanceof Error) {
+				errorToast.message = 'Failed to load posts. Message was: ' + fetchResult.message;
+				toastStore.trigger(errorToast);
+				console.error(fetchResult);
+			} else {
+				toast.message = 'Loaded ' + fetchResult + ' posts';
+				toastStore.trigger(toast);
+			}
+		}
+	}
+
+	async function initiateLoadPost(srcKey: string) {
+		let loadResponse = fetchPost(srcKey, $userStore.token!!, $project.domain);
+		loadResponse.then((r) => {
+			if (r instanceof Error) {
+				errorToast.message = 'Failed to load post';
+				toastStore.trigger(errorToast);
+			} else {
+				toast.message = r;
+				toastStore.trigger(toast);
+				isNewPost = false;
+			}
+		});
+	}
+
+	function initiateNewPost() {
+		let newPost = new MarkdownContent(
+			new PostItem('', '', 'sources/templates/post.html.hbs', '', new Date(), new Date(), true),
+			''
+		);
+		markdownStore.set(newPost);
+		isNewPost = true;
+	}
+
+	async function initiateSavePost() {
+		if ($markdownStore.metadata) {
+			let metadata = $markdownStore.metadata;
+			if (isNewPost || metadata.srcKey === '') {
+				console.log('Is a new post / srcKey is blank, so setting it to the slug', metadata.slug);
+				metadata.srcKey = $project.domain + '/sources/posts/' + metadata.slug + '.md';
+				$markdownStore.metadata = metadata;
+			}
+			let saveResult = await savePost(metadata?.srcKey, $userStore.token!!, $project.domain);
+			if (saveResult instanceof Error) {
+				errorToast.message = 'Failed to save post';
+				toastStore.trigger(errorToast);
+			} else {
+				console.log('postStore: Saved post', metadata.srcKey);
+				isNewPost = false;
+				toast.message = saveResult;
+				toastStore.trigger(toast);
+				reloadPostList();
+			}
+		}
+	}
+
+	async function initiateDeletePost() {
+		console.log('Deleting post');
+		if ($markdownStore.metadata) {
+			let deleteResult = deletePost(
+				$markdownStore.metadata.srcKey,
+				$userStore.token!!,
+				$project.domain
+			);
+			deleteResult.then((r) => {
+				if (r instanceof Error) {
+					errorToast.message = 'Failed to delete post';
+					toastStore.trigger(errorToast);
+				} else {
+					toast.message = 'Deleted post ' + r;
+					markdownStore.set(CLEAR_MARKDOWN);
+					toastStore.trigger(toast);
+					reloadPostList();
+				}
+			});
+		}
+	}
+
+	async function reloadPostList() {
+		postListNodes = [];
+		posts.set({ count: -1, posts: [] });
+		tick();
+		await loadPostList();
+	}
+
+	const userStoreUnsubscribe = userStore.subscribe((value) => {
+		if (value) {
+			// do nothing
+		}
+	});
+
+	const postsUnsubscribe = posts.subscribe((value) => {
+		if (value && value.count != -1) {
+			// build TreeViewNodes from PostNodes
+			for (const post of value.posts) {
+				postListNodes.push({
+					id: post.srcKey,
+					content: PostListItem,
+					contentProps: { title: post.title, date: post.date, srcKey: post.slug }
+				});
+			}
+			postListNodes = [...postListNodes];
+		}
+	});
+
+	const contentStoreUnsubscribe = markdownStore.subscribe((value) => {
+		if (value) {
+			if (value.metadata != null) {
+				pgTitle = value.metadata.title;
+			}
+		}
+	});
+</script>
 
 <svelte:head>
 	<title>Cantilever: Blog Posts {webPageTitle}</title>
